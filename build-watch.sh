79da8c4deaefd0053a881bb29d4829722b4531f9
Sean D Gillespie <sean@mistersg.net>
2019-12-30 21:24:44 -0500

    Add a watch/build script

---
+++ build-watch.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+options=$(getopt -o h --long help -- "$@")
+
+eval set -- "$options"
+
+function print_help {
+    cat <<EOF
+Build and run tests when files change.
+
+Usage: $0 [--] [TEST_TARGETS]
+EOF
+
+    exit 1
+}
+
+case $1 in
+    -h|--help)
+        print_help
+        shift
+        ;;
+    --)
+        shift
+        ;;
+esac
+
+watchman-make \
+    --make "cabal --test-show-details=direct" \
+    --pattern "*.cabal" "**/*.hs" --target "test $@" \
+    --settle 2
+exit $?
