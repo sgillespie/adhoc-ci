18bf445156f593db714739f7471bd2189b3c310a
Sean D Gillespie <sean@mistersg.net>
2020-01-01 21:58:23 -0500

    Implement Commands in Main
    With this, the application is now completely functioning

--- src/Development/AdhocCi.hs
+++ src/Development/AdhocCi.hs
@@ -1,12 +1,18 @@
-module Development.AdhocCi (
-  PipelineOpts (..),
-  mkPipelineOpts,
-  runStages,
-  module Development.AdhocCi.Config
+module Development.AdhocCi
+  ( PipelineOpts (..),
+    mkPipelineOpts,
+    runStages,
+    module Development.AdhocCi.Commands,
+    module Development.AdhocCi.Config
   ) where
 
+import Control.Monad (forM_)
 import qualified Data.ByteString as B
 import Data.Yaml (encode, prettyPrintParseException)
+import System.Exit (ExitCode (..), exitWith)
+import System.FilePath
+
+import Development.AdhocCi.Commands
 import Development.AdhocCi.Config
 
 data PipelineOpts = PipelineOpts
@@ -23,9 +29,18 @@ mkPipelineOpts = PipelineOpts
   }
 
 runStages :: PipelineOpts -> IO ()
-runStages opts = do
-  result <- parseConfigFile configPath
-  case result of
-    Left err      -> putStr (prettyPrintParseException err)
-    Right result' -> B.putStr (encode result')
-  where configPath = rootPath opts ++ "/" ++ configFile opts
+runStages PipelineOpts{configFile=conf, rootPath=root, stage=stages'} = do
+  let configPath = root </> conf
+
+  config  <- parseConfigFile configPath
+  config' <- case config of
+    Left err      -> do
+      putStr (prettyPrintParseException err)
+      exitWith (ExitFailure 1)
+    Right result -> return result
+
+  let cStages = filter (\s -> name s `elem` stages') (stages config')
+
+  forM_ cStages $ \ Stage{commands=cs, name=n} -> do
+    putStrLn $ "Running stage " ++ n
+    mapM_ (runCommand root) cs
