d2e9ef004dad46363aad09a617d7c26ad51deb10
Sean D Gillespie <sean@mistersg.net>
2020-03-05 21:32:58 -0500

    Add pretty console printing

--- src/Development/AdhocCi.hs
+++ src/Development/AdhocCi.hs
@@ -10,12 +10,14 @@ module Development.AdhocCi
 
 import Control.Exception (throw)
 import Control.Monad (forM_)
-import Prelude hiding (putStr, putStrLn)
+import Prelude hiding ((<$>), putStr, putStrLn)
 import Data.Yaml (prettyPrintParseException)
 import System.Exit (ExitCode (..))
 import System.FilePath
+import Text.PrettyPrint.ANSI.Leijen hiding ((</>), putDoc)
 import qualified Prelude as P
 import qualified System.Exit as E
+import qualified Text.PrettyPrint.ANSI.Leijen as L
 
 import Development.AdhocCi.Commands
 import Development.AdhocCi.Config
@@ -34,11 +36,13 @@ mkPipelineOpts = PipelineOpts
   }
 
 class Monad m => MonadPrint m where
+  putDoc :: Doc -> m ()
   putStr :: String -> m ()
   putStrLn :: String -> m ()
   exitWith :: ExitCode -> m a
 
 instance MonadPrint IO where
+  putDoc = L.putDoc
   putStr = P.putStr
   putStrLn = P.putStrLn
   exitWith = E.exitWith
@@ -66,40 +70,54 @@ runStages PipelineOpts{configFile=conf, rootPath=root, stage=stages'} = do
 
   -- Run before_all hook
   runHook root beforeAll' $
-    putStrLn "Running before_all hook"
+    putDocLn $ boldYellow (text "Running before_all hook")
   
   -- Run the stages' jobs
   let cStages = filter (`elem` stages') (stages config')
   forM_ cStages $ \stage' -> do
     -- Run before_each hook
     runHook root beforeEach' $
-      putStrLn "Running before_each hook"
+      putDocLn $ boldYellow (text "Running before_each hook")
 
     -- Run the jobs
-    putStrLn $ "Running stage " ++ stage'
+    putDocLn $ boldYellow (text "Running stage" <+> text stage')
     runStage root stage' (jobs config')
 
     -- Run after_each hook
     runHook root afterEach' $
-      putStrLn "Running after_each hook"
+      putDocLn $ boldYellow (text "Running after_each hook")
 
   -- Run after_all hook
   runHook root afterAll' $
-    putStrLn "Running after_all hook"
+    putDocLn $ boldYellow (text "Running after_all hook")
 
-runHook :: (MonadProcess m) => FilePath -> Maybe [String] -> m () -> m ()
+  putDocLn $ boldYellow (text "Job completed successfully")
+
+runHook :: (MonadProcess m, MonadPrint m, MonadProcess m)
+        => FilePath
+        -> Maybe [String]
+        -> m ()
+        -> m ()
 runHook dir (Just cmds) action = action >> mapM_ (runCommand' dir) cmds
 runHook _ _ _                  = return ()
 
-runStage :: MonadProcess m => FilePath -> String -> [Job] -> m ()
+runStage :: (MonadProcess m, MonadPrint m) => FilePath -> String -> [Job] -> m ()
 runStage dir stage' js
   = forM_ jobs' $ \ Job{commands=cs} ->
       forM_ cs (runCommand' dir)
   where jobs' = filter (\ Job{stage=s} -> s == stage') js
 
-runCommand' :: MonadProcess m => FilePath -> String -> m ()
+boldYellow :: Doc -> Doc
+boldYellow = bold . yellow
+
+putDocLn :: MonadPrint m => Doc -> m ()
+putDocLn d = putDoc $ d <$> empty
+
+runCommand' :: (MonadProcess m, MonadPrint m) => FilePath -> String -> m ()
 runCommand' dir cmd = do
   let cmd' = (mkCommand cmd) { workDir = dir }
+
+  putDocLn $ yellow (text "$" <+> text cmd)
   
   res <- runCommand cmd'
   case res of
--- test/Development/AdhocCiSpec.hs
+++ test/Development/AdhocCiSpec.hs
@@ -10,12 +10,14 @@ import Control.Monad.Writer
 import Data.Yaml (ParseException(..))
 import System.Exit (ExitCode(..))
 import Test.Hspec hiding (beforeAll, afterAll)
+import Text.PrettyPrint.ANSI.Leijen (Doc(..))
 
 import Development.AdhocCi
 
 data FixtureInst m = FixtureInst
   { _runCommand :: Command -> m ExitCode,
     _parseConfigFile :: FilePath -> m (Either ParseException Config),
+    _putDoc :: Doc -> m (),
     _putStr :: String -> m (),
     _putStrLn :: String -> m (),
     _exitWith :: forall a. ExitCode -> m a
@@ -25,6 +27,7 @@ mkFixtureInst :: FixtureInst m
 mkFixtureInst = FixtureInst
   { _runCommand = error "unimplemented instance method '_runCommand'",
     _parseConfigFile = error "unimplemented instance method '_parseConfigFile'",
+    _putDoc = error "unimplemented instance method '_putDoc'",
     _putStr = error "unimplemented instance method '_putStr'",
     _putStrLn = error "unimplemented instance method '_putStrLn'",
     _exitWith = error "unimplemented instance method '_exitWith'"
@@ -53,6 +56,10 @@ instance Monoid l => MonadConfigFile (TestM l) where
     f file
 
 instance Monoid l => MonadPrint (TestM l) where
+  putDoc d = do
+    f <- asks _putDoc
+    f d
+  
   putStr s = do
     f <- asks _putStrLn
     f s
@@ -82,6 +89,7 @@ spec = describe "runStages" $ do
     let fixture = mkFixtureInst
           { _parseConfigFile = \_ -> return (Right config'),
             _runCommand = \cmd -> tell [cmd] >> return ExitSuccess,
+            _putDoc = \_ -> return (),
             _putStrLn = \_ -> return (),
             _putStr = \_ -> return ()
           }
@@ -97,6 +105,7 @@ spec = describe "runStages" $ do
     let fixture = mkFixtureInst
           { _parseConfigFile = \_ -> return (Right config'),
             _runCommand = \cmd -> tell [cmd] >> return ExitSuccess,
+            _putDoc = \_ -> return (),
             _putStrLn = \_ -> return (),
             _putStr = \_ -> return ()
           }
